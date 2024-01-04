ceedb77a59a098951e91e34ba0f6721eb30e8882
Manuel Tonello <49204166+txnello@users.noreply.github.com>
2024-01-04 21:43:55 +0100

    social proof project

---
+++ social-proof-section-master/script.js
@@ -0,0 +1,12 @@
+let resizeObserver = new ResizeObserver(() => {
+    // double check to avoid flickering
+    if (window.innerWidth > 1300 && document.getElementById("theme").getAttribute("href") === "styles/mobileStyle.css") {
+        document.getElementById("theme").setAttribute("href", "styles/desktopStyle.css");
+    }
+
+    if (window.innerWidth <= 1300 && document.getElementById("theme").getAttribute("href") === "styles/desktopStyle.css") {
+        document.getElementById("theme").setAttribute("href", "styles/mobileStyle.css");
+    }
+}); 
+
+resizeObserver.observe(document.getElementsByTagName("body")[0]);
